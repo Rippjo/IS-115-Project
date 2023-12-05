bb1161e02377d3ee7c84f07af1d31037ae6fdecc
Rippjo <Kasperforum@hotmail.no>
2023-12-05 18:41:01 +0100

    calendar

--- WWW/assets/inc/calendar_student.inc.php
+++ WWW/assets/inc/calendar_student.inc.php
@@ -11,25 +11,34 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 // Include your database connection file
 require_once "../../../Private/dbConn.php";
 
-// Fetch available hours from the database
-$sql = "SELECT * FROM teacher_availability WHERE is_booked = 0";
+// Fetch available hours with teacher information from the database
+$sql = "SELECT ta.*, u.name AS teacher_name, u.user_id, ta.course_name
+        FROM teacher_availability ta
+        JOIN user u ON ta.teacher_id = u.user_id
+        WHERE ta.is_booked = 0";
+
 $result = $conn->query($sql);
 
-// Create an array to store available hours by day
-$availableHoursByDay = [];
+// Create an array to store available hours by day and teacher
+$availableHoursByDayAndTeacher = [];
 
 // Check if there are available hours
 if ($result->num_rows > 0) {
-    // Organize available hours by day
+    // Organize available hours by day and teacher
     while ($row = $result->fetch_assoc()) {
         $dayOfWeek = $row['day_of_week'];
         $startTime = $row['start_time'];
         $endTime = $row['end_time'];
+        $teacherName = $row['teacher_name'];
+        $teacherId = $row['user_id'];
+        $courseName = $row['course_name'];
 
         // Store the available hours in the array
-        $availableHoursByDay[$dayOfWeek][] = [
+        $availableHoursByDayAndTeacher[$dayOfWeek][$teacherId][] = [
             'start_time' => $startTime,
             'end_time' => $endTime,
+            'teacher_name' => $teacherName,
+            'course_name' => $courseName,
         ];
     }
 }
@@ -60,13 +69,18 @@ $conn->close();
             echo "<h3>$day</h3>";
 
             // Check if there are available hours for the day
-            if (isset($availableHoursByDay[$day])) {
+            if (isset($availableHoursByDayAndTeacher[$day])) {
                 // Display available hours
-                echo "<div class='available-hours'>";
-                foreach ($availableHoursByDay[$day] as $hours) {
-                    echo "<p>{$hours['start_time']} - {$hours['end_time']}</p>";
+                foreach ($availableHoursByDayAndTeacher[$day] as $teacherId => $teacherHours) {
+                    echo "<div class='teacher-column'>";
+                    echo "<h4>{$teacherHours[0]['teacher_name']} - {$teacherHours[0]['course_name']}</h4>";
+                    echo "<div class='available-hours'>";
+                    foreach ($teacherHours as $hours) {
+                        echo "<p>{$hours['start_time']} - {$hours['end_time']}</p>";
+                    }
+                    echo "</div>";
+                    echo "</div>";
                 }
-                echo "</div>";
             } else {
                 echo "<p class='no-available-hours'>No available hours</p>";
             }
